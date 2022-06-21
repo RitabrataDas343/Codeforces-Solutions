1e4272d230bad877e566192ec38abe77dbdc4505
Ritabrata Das <dasritabrata5@gmail.com>
2022-06-21 14:54:16 +0530

    added template

---
+++ template.cxx
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+typedef long long int lli;
+typedef long long ll;
+typedef long double ld;
+typedef int_fast64_t fast64;
+
+#define IO                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+    
+#define ln(x) x.length()
+#define pi 3.1415926535
+#define f(i, a, b) for (lli i = a; i < b; i++)
+#define rf(i, a, b) for (lli i = a; i >= b; i--)
+#define all(x) x.begin(), x.end()
+
+#define Yes cout << "YES" \
+                 << "\n"
+#define No cout << "NO" \
+                << "\n"
+#define auto_init(v, x) \
+    for (auto &x : v)   \
+        cin >> x;
+#define auto_display(v, x) \
+    for (auto x : v)       \
+        cout << x << " ";  \
+    cout << endl;
+#define auto_displayln(v, x) \
+    for (auto x : v)         \
+        cout << x << endl;
+    
+#define vi vector<int>
+#define mpi map<int, int>
+#define mpll map<ll, ll>
+#define vll vector<ll>
+#define vst vector<string>
+#define pii pair<int, int>
+#define inf INT64_MAX
+
+const lli mod = 1e18;
+    
+using namespace std;
+    
+int main(){
+    IOS_FASTAF
+    // Write your code here
+    return 0;
+}
